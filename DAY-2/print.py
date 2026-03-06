251edd3e7cabb5cd068636e1698283be52b7c687
psaisharan10-source <psaisharan10@gmail.com>
2026-03-06 17:27:57 +0530

    Add files via upload

---
+++ DAY-2/print.py
@@ -0,0 +1,54 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Thummala Sudhakar/Desktop/Python full stack course work/Day-1/input.py
+a = 10
+b= 20.3
+c= "Python"
+a
+10
+b
+20.3
+c
+'Python'
+print(a,b,c)
+10 20.3 Python
+print("a=",a,"b=",b,'c=',c)
+a= 10 b= 20.3 c= Python
+print("a=",a,"b=",b,'c=',c,sep='')
+a=10b=20.3c=Python
+print("a=",a,"b=",b,'c=',c,sep='\n')
+a=
+10
+b=
+20.3
+c=
+Python
+print("a=",a,"b=",b,'c=',c,sep='\n',end='\n\n')
+a=
+10
+b=
+20.3
+c=
+Python
+
+print("a=",a,"b=",b,'c=',c,sep=',,,,',end='......')
+a=,,,,10,,,,b=,,,,20.3,,,,c=,,,,Python......
+print(f"a= {a} b= {b} c= {c}")
+a= 10 b= 20.3 c= Python
+print("a = %d b= %f c= %s"%(a,b,c))
+a = 10 b= 20.300000 c= Python
+print("a={}, b={}, c={}".format(a,b,c))
+a=10, b=20.3, c=Python
+print("a={1}, b={2}, c={0}".format(a,b,c))
+a=20.3, b=Python, c=10
+>>> 
+= RESTART: C:/Users/Thummala Sudhakar/Desktop/Python full stack course work/Day-1/input.py
+------------Personal Information----------------
+Student ID: CGH2884
+Batch No: PFS-HYD-050
+Email ID: tejav7236@gmail.com
+------------Academic Information----------------
+Department: B.Tech - Electronics and Communication Engineering (ECE)
+College Name: Gnanamani college of technology
+Qualification: UG (Bachelor Degrees)
---
+++ DAY-2/student_info.py
@@ -0,0 +1,77 @@
+Student_Name='P.SaiSharan'
+Student_ID='CGH2922'
+Batch_No='PFS-HYD-050'
+Email_ID='psaisharan10@gmail.com'
+Date_of_Birth='2005-01-10'
+Age='21'
+Gender='Male'
+Blood_Group='B+'
+City='karimnagar'
+State='telangana'
+Student_Phone_Number='+917989837450'
+Parent_Phone_Number='+919959550382'
+Github_Link='https://github.com/psaisharan10'
+
+Academic_Information
+
+College_Name='cmr collage of engineering and technology'
+USN_Number='22h51a62b3'
+Qualification='UG (Bachelor Degrees)'
+Department='B.Tech - Computer Science (CSE, Cybersecurity, AI & ML, IoT, Cloud Computing, Data Analytics, Data Science)'
+Pass_out_Year='2026'
+Graduation_Percentage='65%'
+Arrears='Yes'
+No_of_Arrears='2'
+10th_Pass_Year='2020'
+10th_Percentage='93%'
+12th_Pass_Year='2022'
+12th-Percentage='60%'
+Skills='Python, Flask, HTML, CSS, Bootstrap, Javascript, MySQL'
+
+print('--------Personal information')
+
+print(f'Student Name: {Student_Name}')
+print(f'Student ID: {Student_ID}')
+print(f'Batch No :  {Batch_No}')
+print(f'Email ID :  {Email_ID}')
+print(f'Date of Birth :  {Date_of_Birth}')
+print(f'Age :  Age{}')
+print(f'Gender :  {Gender}')
+print(f'Blood Group :  {Blood_Group}')
+print(f'City :  {City}')
+print(f'State :  {State}')
+print(f'Student Phone Number :  {Student_Phone_Number}')
+print(f'Parent Phone Number :  {Parent_Phone_Number}')
+print(f'Github Link :  {Github_Link}')
+
+
+
+
+print('--------Academic information')
+print(f'College Name : {College_Name}')
+print(f'USN Number : {USN_Number}')
+print(f'Qualification : {Qualification}')
+print(f'Department : {Department}')
+print(f'Pass out Year : {Pass_out_Year}')
+print(f'Graduation Percentage : {Graduation_Percentage}')
+print(f'Arrears : {Arrears}')
+print(f'No of Arrears : {No_of_Arrears}')
+print(f'10th Pass Year: {10th_Pass_Year}')
+print(f'10th Percentage : {10th_Percentage}')
+print(f'12th Pass_Year: {12th_Pass_Year}')
+print(f'12th Percentage: {12th-Percentage}')
+print(f'Skills: {Skills}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
